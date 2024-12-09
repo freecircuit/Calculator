b2df9548a3d23469105b35f4b2df71daf3f36939
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-12-09 00:32:55 -0800

    populate display and array

--- Calculator.js
+++ Calculator.js
@@ -1,8 +1,43 @@
 // Calculator variables
-var num1 = a
-var op = b
-var num2 = c
-const solution = operate(op, num1, num2)
+const values = ['0', '0'];
+var op = document.querySelector('.ops');
+let num = document.querySelector('.nums');
+let display = document.querySelector("#InnerDisplay");
+let count = 0;
+
+//Retrieve values for variables
+num.addEventListener('click', (E) => {
+  num = E.target;
+  display.textContent += num.innerHTML;
+  });
+
+  op.addEventListener('click', (e) => {
+    values[count] = parseFloat(display.innerHTML)
+    console.log(values[count])
+    count += 1;
+    if(count > 1){
+      count = 0
+    }
+    console.log(count)
+    op = e.target
+    let opr = op.innerHTML
+    console.log(opr)
+    display.textContent = null
+   console.log(values)
+
+  })
+
+  
+  
+  
+
+
+
+    // if(values[1] != '0'){
+    // display.textContent = values.reduce((total, currentItem) => {
+    //   return operate(opr, parseFloat(currentItem), parseFloat(total))
+    // })}
+
 
 //The operate function:
 const operate = function (op, num1, num2) {
@@ -25,6 +60,15 @@ const operate = function (op, num1, num2) {
   else if (op === '^'){
     const num = power(num1, num2);
     return num;
+  }
+  else if (op == '%'){
+    return num1 % num2;
+  }
+  else if (op == 'sqrt'){
+    return Math.sqrt(num1, num2);
+  }
+  else if (op == 'C'){
+    return 
   }
 }
 
