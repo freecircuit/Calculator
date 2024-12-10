b69bd28d5337b0a82864e6aeb9370f27d51ce933
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-12-09 18:41:51 -0800

    finish Calculator.

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,13 @@
 // Calculator variables
 const values = [];
-var op = document.querySelectorAll('.op');
-let num = document.querySelectorAll('.num');
-let display = document.querySelector("#InnerDisplay");
-let equal = document.querySelector('#equal')
+const op = document.querySelectorAll('.op');
+const num = document.querySelectorAll('.num');
+const display = document.querySelector("#InnerDisplay");
+const equal = document.querySelector('#equal')
+const clear = document.querySelector('#clear')
 let count = 1;
 let opp = {rator: null}
-const temp = {temp: null}
+let temp = {temp: null}
 let answer = 0;
 let string = '';
 let click = 0;
@@ -14,14 +15,26 @@ let click = 0;
 //Retrieve values for variables
 document.querySelectorAll(".num").forEach(item => {
   item.addEventListener('click', (e) => {
+    //Clear display
     if (click == 0){
       display.textContent = null
     }
     click += 1;
+    if(count > 2){
+      string = '0'
+      opp = {rator: null}
+      temp = {temp: null}
+      answer = 0;
+      count = 1
+    }
+    //Add string of numbers to display
     if (click > 0){
     display.textContent += e.target.innerHTML;}
 
+    //populate variable to be added to values array
     string += e.target.innerHTML
+
+
   }
   )
   
@@ -30,13 +43,18 @@ document.querySelectorAll(".num").forEach(item => {
   //Assign operator to a variable and use the click event as to begin next number    
   document.querySelectorAll('.op').forEach(item => {
       item.addEventListener('click', (e) => {
-        if(display.textContent == null){
-          return;
-        }
+
+        //Capture operator variable from last time through for calculation
         temp.temp = opp.rator
         console.log(temp.temp)
+
+        //Populate values array with number from calc
         values[count] = parseFloat(string.valueOf())
+
+        //Create variable from operator
         opp.rator = e.target.innerHTML
+
+        //Reorganize array inorder to calculate on correct numbers
         if(count < 2){
           values[0] = values[1]
         } 
@@ -44,17 +62,52 @@ document.querySelectorAll(".num").forEach(item => {
         count += 1;
          
         if(count > 2){
+
         answer = operate(temp.temp, values[0], values[2]);
         values[0] = answer;
         count = 2
       }
         console.log(opp.rator)
         console.log(answer)
+
+        //Round to four decimal places and reset temp values
         display.textContent = Math.round(answer * 10000) / 10000;
         string = '0'
         click = 0
         console.log(values)
       })
+    })
+
+   
+ //Code equals button
+    equal.addEventListener('click', (e) => {
+      if(count < 2){
+        values[0] = parseFloat(string.valueOf())
+        display.textContent = values[0]
+        }
+      else{
+        values[count] = parseFloat(string.valueOf())
+        answer = operate(opp.rator, values[0], values[2])
+        display.textContent = Math.round(answer * 10000) / 10000;
+        values[0] = answer
+        count += 1
+        opp = {rator: null}
+        temp = {temp: null}
+        answer = 0;
+      }
+        string = '0'
+        click = 0
+        
+    })
+    //Add functionality to the clear button
+    clear.addEventListener('click', (e) => {
+      count = 1;
+      opp = {rator: null}
+      temp = {temp: null}
+      answer = 0;
+      string = '';
+      click = 0;
+      display.textContent = null
     })
 
 
